5223c5b34e304d781bf34f26b1408172f85b7347
Gustavo4089 <gustavolucena4089@gmail.com>
2024-11-23 14:43:36 -0300

    Tipos Variaveis

--- application.js
+++ application.js
@@ -1,4 +1,44 @@
-console.log("Olá mundo !!!!")
 
 
-window.alert("Olá mundo !!!!")
+// Variáveis no Java Script
+
+// var = Variável Global
+// let = Variável no escopo
+// const = Variável Constante (Não pode ser moodificada)
+
+var nome = "Rafael"
+
+nome = "Gustavo"
+
+console.log(nome)
+
+
+{
+    let idade = 24
+    console.log(idade)
+}
+
+
+const texto = "Meu nome é "+nome;
+
+
+console.log(texto)
+
+// Tipo de dados
+// String
+let n = "Gustavo"
+console.log("content: "+n+" type: string")
+
+// int
+let age = 26
+console.log("content: "+age+" type: int")
+
+// float
+let height = 1.75
+console.log("content: "+height+" type: float")
+
+// boolean
+let logged = false
+console.log("content: "+logged+" type: boolean")
+
+
