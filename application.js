c562df3ade0f1cd9f7bf4bb5cbaf138345dc70cd
gustadev4089 <gustavolucena4089@gmail.com>
2024-12-09 22:53:54 -0300

    Laço ForEach

--- application.js
+++ application.js
@@ -1,19 +1,11 @@
 
-let voltas = 10;
+let lista_nomes = [
+    "Gustavo",
+    "Olavo",
+    "Mario",
+    "Rafael"
+];
 
-
-while(voltas > 0){
-    console.log(`While --> ${voltas}`)
-    voltas--;
-}
-
-for(let loops = 1; loops <= 10; loops++){
-    console.log(`For --> ${loops}`)
-}
-
-let loops = 0
-do{ // <-- Nesse tipo de função primeiro é executado o contêudo para depois fazer a verificação
-    loops++;
-    console.log(`Do While --> ${loops}`)
-}while(loops < 10);
-
+lista_nomes.forEach((nome, index)=>{
+    console.log(`Na posição ${index} tem o valor ${nome}`)
+})
