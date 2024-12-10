8d29b6141a8b29c7cc740dc07501207bd2328108
gustadev4089 <gustavolucena4089@gmail.com>
2024-12-09 22:45:35 -0300

    Laços de repetição

--- application.js
+++ application.js
@@ -1,39 +1,19 @@
 
+let voltas = 10;
 
-function get_all_info(){
-    const title_message = document.querySelector("#field_title_message");
-    const message = document.querySelector("#field_message");
-    const color_text = document.querySelector("#field_color");
 
-    add_li(title_message.value, message.value);
-
-    change_color(color_text.value)
+while(voltas > 0){
+    console.log(`While --> ${voltas}`)
+    voltas--;
 }
 
-
-function change_color(color){
-    const element_content = document.querySelector(".section2-content ul")
-    element_content.style.color = `${color}`
+for(let loops = 1; loops <= 10; loops++){
+    console.log(`For --> ${loops}`)
 }
 
+let loops = 0
+do{ // <-- Nesse tipo de função primeiro é executado o contêudo para depois fazer a verificação
+    loops++;
+    console.log(`Do While --> ${loops}`)
+}while(loops < 10);
 
-function add_li(title, message){
-    const element_ul = document.querySelector(".section2-content ul")
-
-    element_ul.innerHTML += `<li> <h3>${title}</h3> <p>${message}</p> </li>`
-}
-
-
-// Declarar uma função dentro de uma variável
-
-var conta = function(a, b){
-    return a + b
-}
-
-console.log(`Função conta --> ${conta(10, 50)}`);
-
-
-
-(function(){
-    console.log("Função chamada no momento que é declarada")
-})()
