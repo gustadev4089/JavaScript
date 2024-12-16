d62e265a331534d714e2c693e5be81c1b5ef362a
gustadev4089 <gustavolucena4089@gmail.com>
2024-12-16 13:30:25 -0300

    Orientação a objeto

--- application.js
+++ application.js
@@ -1,29 +1,64 @@
 
+function running(){
+
+    let pessoa1 = {
+        nome: "Gustavo",
+        idade: 26,
+        cpf: "745.757.189-34",
+        RG: "67.453.213-X",
+    }
+
+    let pessoa2 = {
+        "nome":"Guilherme",
+        "idade": 24,
+        "cpf": "123.456.789-12",
+        "RG": "12.123.456-A"
+    }
+
+
+    let pessoas = [
+        {
+            nome: "Thais",
+            idade: 23,
+            cpf: "123.456.789-00",
+            RG: "12.345.678-B"
+        },
+
+        {
+            nome: "Rogerio",
+            idade: 20,
+            cpf: "123.456.789-00",
+            RG: "12.345.678-B"
+        },
+
+        {
+            nome: "Marcelo",
+            idade: 30,
+            cpf: "123.456.789-00",
+            RG: "12.345.678-B"
+        }
+    ]
+
+    // Extraindo uma variável de um objeto
+    let {nome} = pessoa1;
+
+    console.log(nome)
+
+    // Mostrando o contêudo da variável
+    console.log(pessoa2.RG)
+
+    // Usando FOR loop para percorrer a lista de objetos
+    for(let index=0; index < pessoas.length; index++){
+        console.log(pessoas[index])
+    }
+
+    pessoas.forEach(element => {
+        console.log(`forEach --> ${element.nome}`)
+    });
+
+}
 
 
 window.addEventListener("load", ()=>{
-
-    let expression = ""
-
-
-    let termo_1 = prompt("Digite o primeiro termo")
-    let sinal = prompt("Digite o sinal")
-    let termo_2 = prompt("Digite o segundo termo")
-
-    expression = (termo_1+sinal+termo_2)
-
-    switch(sinal){
-        case "+":
-            console.log(`${expression} = ${(Number(termo_1))+(Number(termo_2))}`)
-            break;
-        case "-":
-            console.log(`${expression} = ${(Number(termo_1))-(Number(termo_2))}`)
-            break;
-        case "*":
-            console.log(`${expression} = ${(Number(termo_1))*(Number(termo_2))}`)
-            break;
-        case "/":
-            console.log(`${expression} = ${(Number(termo_1))/(Number(termo_2))}`)
-            break;
-    }
+    running();
 })
