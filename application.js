fd1bb3bfb49703163c9045b519b873cbb5561daa
gustadev4089 <gustavolucena4089@gmail.com>
2024-12-27 09:16:17 -0300

    Manipulando DOM pela Tag

--- application.js
+++ application.js
@@ -1,64 +1,7 @@
 
 
-class Tempo{
-    constructor(){
-        let week = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"];
+let elements = document.querySelectorAll("p");
 
-        let month = [
-            "Janeiro",
-            "Fevereiro",
-            "Março",
-            "Abril",
-            "Maio",
-            "Junho",
-            "Julho",
-            "Agosto",
-            "Setembro",
-            "Outubro",
-            "Novembro",
-            "Dezembro"
-        ];
-
-        let date = new Date();
-
-        this.dia_semana = week[Number(date.getDay() - 1)]
-        this.mes_ano = month[date.getMonth()]
-        
-        this.dia = date.getDate();
-        this.mes = Number(date.getMonth()) + 1;
-        this.ano = date.getFullYear();
-        this.data_completa = String(`${this.dia}/${this.mes}/${this.ano}`);
-    }
-
-
-    show_week_day = ()=>{
-        console.log(`hoje é ${this.dia_semana}`);
-    }
-
-    show_month_year = ()=>{
-        console.log(`Mês ${this.mes_ano}`);
-    }
-
-}
-
-
-addEventListener("load", ()=>{
-    
-    let hoje = new Tempo();
-
-    let element_p = document.querySelector("p");
-    let element_h3 = document.querySelector("h3");
-
-    element_p.innerHTML = `${hoje.data_completa}`;
-
-    element_h3.innerHTML = `Hoje é ${hoje.dia_semana}, ${hoje.dia} de ${hoje.mes_ano}.` 
+elements.forEach((element, index)=>{
+    element.innerHTML += " "+(index+1);
 })
-
-
-
-
-
-let hoje = new Tempo();
-
-hoje.show_week_day();
-hoje.show_month_year();
