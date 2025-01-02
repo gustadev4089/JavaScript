13b6c84c7e6693856449fc9752d56039f44201d5
gustadev4089 <gustavolucena4089@gmail.com>
2025-01-02 14:24:04 -0300

    Promises Async e Await

--- application.js
+++ application.js
@@ -1,40 +1,33 @@
 
-// Spread com uma lista simples
-
-let convidados = ["Gustavo", "Pedro", "Marcio"];
-let novos = ["Matheus", "Otavio", "Marlene", "Miranda"];
-
-let total_pessoas = [...novos, "Maria", "Leticia", "Marcelo", ...convidados];
-
-console.log(total_pessoas);
-
-
-// Spread com lista de objetos
-
-let pessoas_antigas = [
-    {nome: "Marcia", idade: 34, sexo: "feminino"},
-    {nome: "Luiz", idade: 23, sexo: "masculino"},
-    {nome: "Sandra", idade: 30, sexo: "feminino"},
-    {nome: "Marco", idade: 45, sexo: "masculino"},
-]
-
-let nova_lista = [
-    ...pessoas_antigas,
-    {nome: "Mario", idade: 21, sexo: "masculino"},
-    {nome: "Sabino", idade: 60, sexo: "masculino"},
-    {nome: "Leticia", idade: 26, sexo: "feminino"},
-    {nome: "Carla", idade: 28, sexo: "feminino"},
-]
-
-console.log(nova_lista)
-
-
-
-// Rest
-
-const show_convidados = (pessoa, ...outros)=>{
-    let total = [pessoa, ...outros];
-    console.log(`Lista de convidados: ${total}`);
+const req_teste = async (nome)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            if(nome == "Gustavo"){
+                resolve("Nome confirmado com sucesso !!!!");
+            }else{
+                reject("Nome está incorreto !!!");
+            }
+        }, 4000);
+    })
 }
 
-show_convidados("marcio", "Malvino", "Pedro", "Augustus", "Maximos");
+let nome = prompt("Informe o nome do usuário por favor !!!!");
+
+
+
+const running_app = async ()=>{
+    await req_teste(nome).then((resolve)=>{
+        console.log(`o contêudo da promise eh --> ${resolve}`)
+    }).catch((erro)=>{
+        console.log(`a promise foi rejeitada --> ${erro}`)
+    })
+    
+    console.log("testando uma promise !!!!");
+}
+
+
+running_app();
+
+
+
+
