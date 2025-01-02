9c32589deee76ad1f196b6fc337cdd2424f07b09
gustadev4089 <gustavolucena4089@gmail.com>
2025-01-02 19:17:30 -0300

    Requisições usando fetch

--- application.js
+++ application.js
@@ -3,18 +3,34 @@ let title = document.querySelector("h2");
 title.innerHTML = "Carregando ..."
 
 window.addEventListener("load", async()=>{
-    // requisição
-    const request = new XMLHttpRequest();
-    request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
-    
-    request.onload = function(){
-        if(this.status >= 200 && this.status <= 400){
-            title.innerHTML = this.response;
-        }else{
-            alert(`error --> ${this.status}`)
-        }
+
+
+    const users = 30;
+
+    for(let c=1; c <= users; c++){
+        await fetch(`https://jsonplaceholder.typicode.com/todos/${c}`)
+        .then((response) => response.json())
+        .then((json) => console.log(json))
     }
 
-    request.send();
+    console.log("Finished !!!");
+
+
+    await fetch("https://jsonplaceholder.typicode.com/posts",{
+        method: "POST",
+        body: JSON.stringify({
+            title: "Gustavo Dankicode",
+            body: "asmapsmas aspk aspk asa spaspasl,asa",
+            userID: 11,
+            id: 101
+        }),
+        headers:{
+            "Content-type": "application/json, charset=UTF-8",
+        }
+    })
+    .then((response)=> response.json())
+    .then((json)=> console.log(json))
 
 })
+
+
